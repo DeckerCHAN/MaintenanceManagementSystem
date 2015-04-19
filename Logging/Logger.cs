58df043b8e3937aaeacf17abca9ddc8e35a8a83e
DeckerCHAN <DeckerCHAN@gmail.com>
2015-04-20 01:09:05 +0800

    init-commit

---
+++ Logging/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using log4net;
+using log4net.Core;
+
+namespace MMS.Logging
+{
+    public static class Logger
+    {
+        public static ILog GetLogger()
+        {
+            return LogManager.GetLogger(new StackTrace().GetFrame(1).GetMethod().ReflectedType);
+         
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = MMS.Logging.Logger.GetLogger();
+            logger.Info("Hello!");
+            logger.Warn("Hello!");
+            logger.Error("Hello!");
+            Console.WriteLine(ConfigurationManager.AppSettings["countoffiles"]);
+            Console.ReadKey();
+        }
+    }
+}
