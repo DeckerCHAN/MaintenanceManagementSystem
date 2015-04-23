b5c55137f9809afb8f24157c4b37c41ee7cddaed
DeckerCHAN <DeckerCHAN@gmail.com>
2015-04-23 23:57:27 +0800

    Added Persisitence system and EF support

---
+++ Persistence/DBStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace MMS.Persistence
+{
+    public class DBStorage
+    {
+        #region instance
+        private static DBStorage Instance;
+
+        public static DBStorage GetDbStorage()
+        {
+            return Instance ?? (Instance = new DBStorage());
+        }
+        #endregion
+
+        private KeyValueConfigurationCollection Config;
+        private String ConnectingString;
+        private DBStorage()
+        {
+            this.Config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None).AppSettings.Settings;
+            this.ConnectingString = this.Config["DbConnectingString"].Value;
+        }
+    }
+}
---
+++ Persistence/DataEntityBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace MMS.Persistence
+{
+    public static class DataEntityBuilder
+    {
+        public static MMSEntities BuildMmsEntities(String connectString)
+        {
+            return new MMSEntities(connectString);
+        }
+
+        public static MMSEntities BuildMmsEntities()
+        {
+            var configFileMap = new ExeConfigurationFileMap();
+            configFileMap.ExeConfigFilename = "Config\\Persistence.config";
+            Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
+            return BuildMmsEntities(config.ConnectionStrings.ConnectionStrings["MMSEntities"].ConnectionString);
+        }
+
+    }
+}
