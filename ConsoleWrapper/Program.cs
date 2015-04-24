dd0d16cc823f5199f79136d577bda98c1e307580
DeckerCHAN <DeckerCHAN@gmail.com>
2015-04-24 13:02:52 +0800

    Added startup

--- ConsoleWrapper/Program.cs
+++ ConsoleWrapper/Program.cs
@@ -1,14 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using MMS.Core;
 
-namespace ConsoleWrapper
+namespace MMS.ConsoleWrapper
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        private static void Main(string[] args)
         {
+            try
+            {
+                if (Process.GetProcessesByName("MMS").Length > 1)
+                {
+                    MessageBox.Show("MMS is running!", "Error!");
+                    Environment.Exit(-1);
+                }
+
+                var e = Engine.GetEngine();
+                e.Run();
+            }
+            catch (Exception ex)
+            {
+                var report =
+                    new FileInfo(String.Format("crash-report-{0}.report",
+                        DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss-tt")));
+                File.WriteAllText(report.FullName, ex.ToString());
+                Console.WriteLine("!!!CRASH!!!Encountered unhandled exception. System crashed!");
+                Console.WriteLine("!!!CRASH!!!More detail at {0}", report.FullName);
+            }
+            finally
+            {
+                Console.WriteLine("Programme returned...Press any key to exit.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
         }
     }
 }
